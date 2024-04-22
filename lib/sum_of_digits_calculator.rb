f02b07dd58c3943dd20c1eced6ce01856c57f8fd
Alin B <alinbanica@Alins-MacBook-Pro.local>
2024-04-22 10:41:17 +0100

    Initial commit

---
+++ lib/sum_of_digits_calculator.rb
@@ -0,0 +1,9 @@
+# Write a class with a method that takes a number as an input and calculates the sum of its digits, until there is a single digit answer. Include the tests.
+# For examples:
+# When receiving 16 as a parameter it returns 7 (16 --> 1 + 6)
+# When receiving 942 as a parameter it returns 6 (942 --> 9 + 4 + 2 = 15 --> = 1 + 5 = 6)
+# When receiving 9999999999 as a parameter it returns 9 (9999999999 --> 9 + 9 + 9 + 9 + 9 + 9 + 9 + 9 + 9 + 9 + 9 = 99 --> = 9 + 9 = 18 --> = 1 + 8 = 9)
+
+class SumOfDigitsCalculator
+
+end
